4060eea754121bbaf9f5e8d7c11b5666e47bb8e2
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-12-24 03:53:54 +0530

    UPdating files

--- shashi code/ex.py
+++
@@ -1,7 +0,0 @@
-import pywhatkit as kit
-
-number ='+919608182895'
-
-kit.sendwhatmsg(
-    number, 'Happy birthday',19,32
-)
--- shashi code/str_lwr.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-/* Write a string to convert upper case into lower case*/
-void main()
-{
-    char str[]="ABHISHEK";
-    puts(strlwr(str));
-}
