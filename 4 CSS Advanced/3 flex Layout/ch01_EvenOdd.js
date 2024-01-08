8f256bf27f06f283aed625387ebe789dfa28494c
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2024-01-09 02:38:41 +0530

    Even Odd function concept

---
+++ 4 CSS Advanced/3 flex Layout/ch01_EvenOdd.js
@@ -0,0 +1,12 @@
+function evenOdd(num) {
+    if (num % 2 == 0) {
+        return "Even Numver";
+    }
+    else {
+        return "Odd Number";
+    }
+}
+var value = evenOdd(5);
+var value1 = evenOdd(8);
+console.log(value);
+console.log(value1);
