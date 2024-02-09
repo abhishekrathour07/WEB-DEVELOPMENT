508fc6b561bd836f168c4700bbf2029f53e4f416
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2024-02-09 14:20:31 +0530

    JS series

---
+++ 5 JavaScript/1 JS Beginners/Js-Series/2_datatypes.js
@@ -0,0 +1,28 @@
+// data-types and ECMA Standards
+
+"use strict"; // treat all JS code as newer version
+
+let name = "chiranjeev"
+let age = 20
+let isLoggedIn = true
+let state
+
+
+// datatypes
+// number => 2^53
+// bigInt
+// string = ""
+// boolean => true/false
+// null (its stand-alone value)
+// undefined =>
+// symbol => unique
+
+// object
+// typeof
+
+console.log(typeof "namaste");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof null);
+console.log(typeof undefined);
