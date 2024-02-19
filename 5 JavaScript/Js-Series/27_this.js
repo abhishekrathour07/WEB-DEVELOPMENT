6ea44c53ca58c7e79617cbd0215a38af71934239
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2024-02-19 06:36:13 +0530

    Prototype

---
+++ 5 JavaScript/Js-Series/27_this.js
@@ -0,0 +1,21 @@
+// simple function 
+
+function detail(name, id) {
+    console.log(id);
+    console.log(name);
+}
+ detail("Abhi", 123);
+ detail("jai", 222);
+
+// constructor function 
+function consdetail(name, id) {
+    this.name = name;
+    this.id = id; //this talking about current contest
+   
+}
+
+let obj1 =  new consdetail("Abhi", 123)
+console.log(obj1);
+
+let obj2 =  new consdetail("Abhishek", 1234)
+console.log(obj2);
