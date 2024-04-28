7a0faf81b97b328fdc195c5dac34c57cf5e6c8fb
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2024-04-28 23:55:36 +0530

    demo file

---
+++ demo/index.js
@@ -0,0 +1,27 @@
+ let btn = document.querySelectorAll(".button");
+ btn.forEach( function(b){
+     
+     b.addEventListener("click",function(e){
+      console.log(e.target.id);
+        switch(e.target.id){
+
+            case 'red':
+                document.body.style.backgroundColor = e.target.id;
+                break;
+            case 'blue':
+                document.body.style.backgroundColor = e.target.id;
+                break;
+            case 'green':
+                document.body.style.backgroundColor = e.target.id;
+                break;
+            case 'violet':
+                document.body.style.backgroundColor = e.target.id;
+                break;
+            case 'orange':
+                document.body.style.backgroundColor = e.target.id;
+                break;
+        }
+
+     });
+ });
+
