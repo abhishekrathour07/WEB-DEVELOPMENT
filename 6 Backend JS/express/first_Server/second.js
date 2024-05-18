8cfc9c59b1f95a3b30f9f67db23ad8ff1c91f1c7
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2024-05-18 11:03:08 +0530

    Os module

---
+++ 6 Backend JS/express/first_Server/second.js
@@ -0,0 +1,8 @@
+import os from 'os'
+
+console.log(os.freemem())
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.release());
+console.log(os.EOL);
