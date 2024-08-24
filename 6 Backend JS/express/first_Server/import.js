a8c96023ba57cee6f1c74d61d3a927c7bb5bba10
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2024-08-24 16:03:28 +0530

    jquery

--- 6 Backend JS/express/first_Server/import.js
+++
@@ -1,4 +0,0 @@
-import{ dem00s }from './second.mjs';
-
-dem00s();
-console.log('Task executed successfully');
--- 6 Backend JS/node/npmPackages/index.js
+++
@@ -1,8 +0,0 @@
-import generateName from 'sillyname';
-import  superheroes from 'superheroes'
-
-var sillyName = generateName();
-let hero =  superheroes();
-
-console.log(`Hello i'm your ${sillyName}`);
-console.log(`Hello i'm your ${hero}`);
